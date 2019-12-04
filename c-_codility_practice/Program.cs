b27cb5b55cc4881ac4831ce248e1d5bf08a4d055
Andy Laughlin <atl11384@gmail.com>
2019-12-04 09:16:55 +0000

    genomeRangeQuery lesson and minAvgTwoSlce query

---
+++ c-_codility_practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace CodilityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[]{1, 4, 5, 8};
+            var b = new int[]{4, 5, 9, 10};
+            var c = new int[]{4, 6, 7, 10, 2};
+            Solution(a, b, c);
+        }
+
+        public static int Solution(int[] A, int[] B, int[] C)
+        {
+            var planks = A.Zip(B, (a, b) => (a, b));
+            var nails = C.ToImmutableSortedSet();
+
+            return GetNumberNailsRequired(planks, nails);
+        }
+
+        private static int GetNumberNailsRequired(IEnumerable<(int, int)> planks, ImmutableSortedSet<int> nails)
+        {
+            var result = 0;
+            foreach (var plank in planks)
+            {
+                result = Math.Max(result, FindPositionOfNail(nails, plank, result));
+            }
+
+            return result + 1;
+        }
+
+        private static int FindPositionOfNail(ImmutableSortedSet<int> nails, (int, int) plank, int result)
+        {
+            var plankStart = 0;
+            var plankEnd = 1;
+
+            var nailArrayIndex = 0;
+            var nailHitLocation = 1;
+
+            return 0;
+        }
+    }
+}
---
+++ genomic_range_query/GenomicRangeQuery/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace GenomicRangeQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S = "CAGCCTA";
+            var P = new int[3]{2, 5, 0};
+            var Q = new int[3] {4, 5, 6};
+            Solution(S, P, Q);
+        }
+
+        private static int[] Solution(string S, int[] P, int[] Q)
+        {
+            var result = new List<int>();
+            var impactFactors = new Dictionary<string, int>();
+            impactFactors.Add("A", 1);
+            impactFactors.Add("C", 2);
+            impactFactors.Add("G", 3);
+            impactFactors.Add("T", 4);
+
+            var lettersToNumbers = new List<int>();
+
+            var lowestValue = 1;
+            for (var i = 0; i < S.Length; i++)
+            {
+                lettersToNumbers.Add(impactFactors[S[i].ToString()]);
+            }
+            
+            for (var i = 0; i < P.Length; i++)
+            {
+                var positionA = P[i];
+                var positionB = Q[i];
+                if (lettersToNumbers[positionA] == 1 || lettersToNumbers[positionB] == 1)
+                {
+                    result.Add(lowestValue);
+                }
+
+                else if (positionA == positionB)
+                {
+                    result.Add(lowestValue);;
+                }
+
+                else if (positionB - positionA == 1)
+                {
+                    lowestValue = Math.Min(lettersToNumbers[positionA], lettersToNumbers[positionB]);
+                    result.Add(lowestValue);
+                }
+                else
+                {
+                    var arraySliceSize = positionB - positionA;
+                    var arraySlice = lettersToNumbers.Take(arraySliceSize);
+                    var lowestImpactFactor = GetLowestValue(positionA, positionB, lettersToNumbers);
+                    result.Add(lowestImpactFactor);
+                }
+                
+            }
+
+            return result.ToArray();
+        }
+
+
+        private static int GetLowestValue(int positionA, int positionB, List<int> lettersToNumbers)
+        {
+            var result = lettersToNumbers[positionA];
+            for (var i = positionA+1; i < positionB+1; i++)
+            {
+                if (lettersToNumbers[i] == 1)
+                {
+                    return 1;
+                }
+                if (lettersToNumbers[i] < result)
+                {
+                    result = lettersToNumbers[i];
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
